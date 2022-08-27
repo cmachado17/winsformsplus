0d6f8f444ebd740ce15a66447cd3a2a8bda48742
Cristian Machado <56597872+cmachado17@users.noreply.github.com>
2022-08-27 16:46:46 -0300

    manejo de excepciones, try catch, doble catch, algunos tipos de excepciones

---
+++ WinsFormsPlus/excepciones/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace excepciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int a, b, r;
+            try
+            {
+                a = int.Parse(txtNumero1.Text);
+                b = int.Parse(txtNumero2.Text);
+
+                r = a / b;
+
+                lblResultado.Text = "= " + r;
+            }
+            catch (FormatException ex) // como se q va a ser un format excepcion ya pude armar una excepcion pensando en eso
+            {
+                MessageBox.Show("Por favor cargar solo numero");
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("No se puede dividir por 0");
+            }
+        }
+    }
+}
